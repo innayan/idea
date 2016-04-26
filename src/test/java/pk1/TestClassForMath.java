6d14b16740eda24e4f312b9041a1c17c091d7232
innayan <admin@mail.ru>
2016-04-26 13:05:29 +0300

    init

---
+++ src/test/java/pk1/TestClassForMath.java
@@ -0,0 +1,65 @@
+package pk1;
+
+//tjtgjtjtj
+
+import junit.framework.*;
+
+//dssssss
+public class TestClassForMath extends TestCase {
+
+        public void testMultiply() {
+            ClassForMath tester = new ClassForMath();
+               Assert.assertEquals("Result", 3, tester.multiply(6, 5));
+
+        }
+        public void testMultiply2() {
+            ClassForMath tester = new ClassForMath();
+               Assert.assertEquals("Result", 3, tester.multiply2(6, 5));
+
+        }
+
+
+public void testSumma() {
+
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 7, tester.summa(12, 5));
+
+}
+public void testDifference() {
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 18, tester.difference(12, 5));
+
+}public void testDifference2() {
+
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 118, tester.difference(12, 5));
+   //ggggggggggggggggggggg
+}
+public void testDifference1() {
+
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result",7 , tester.difference(12, 5));
+
+}
+public void testDivision() {
+    ClassForMath tester = new ClassForMath();
+    //dfgdgfdfg
+       Assert.assertEquals("Result", 7, tester.division(8,1));}
+
+public void testDoubleSumma() {
+	//dfgfgdg/dfgfg/df/gf/g/dgf
+	///ffffffff
+	//dfdfdfsfsdfsfdsfsf
+	//dfsfdfsdfdfsdfsff
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 34, tester.double_summa(12, 5));
+
+}
+public void testTripleSumma()
+{ClassForMath test1= new ClassForMath();
+Assert.assertEquals("Result", 12, test1.triple_summa(2,2) );
+	}
+
+
+}
+
